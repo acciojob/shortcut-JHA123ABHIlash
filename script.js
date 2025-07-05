b477965acc78013578bb8eaad19d8593d858a6a2
Abhilash Jha <152012438+JHA123ABHIlash@users.noreply.github.com>
2025-07-05 10:20:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function shortcut(s1, s2) {
   // your code here
+	let op="";
+	let str1=s1[0];
+	let str2=s2[0];
+	op=str1+str2;
+	if(str1===undefined || str2===undefined){
+		console.log("''");
+	}else {
+		console.log(op);
+	}
 }
 
 // Do not change the code below.
-const s1 = prompt("Enter s1:");
+const s1 = prompt("Enter s1:"); 
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
